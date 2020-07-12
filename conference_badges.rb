e5d021be645dbc4e2a4c90b2035118989b02d836
Clayton Moore <cmoorefour@gmail.com>
2020-07-12 00:18:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,7 +20,7 @@ end
 
 
 def printer(attendees)
-   batch_badge_creator(attendees).each do |assign_rooms|
+   batch_badge_creator(attendees).each do |attendee|
     puts badge_maker (attendees)
    end
 end
